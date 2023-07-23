25c155387191b593428ecaf52b3d9c93779aec25
Edwin Cardenas <eacardenase@unal.edu.co>
2023-07-22 20:44:56 -0500

    feat: add login form validation

--- lib/widgets/login_form.dart
+++ lib/widgets/login_form.dart
@@ -13,9 +13,24 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit() {
+    final isValid = _formKey.currentState!.validate();
+
+    if (isValid) {
+      _formKey.currentState!.save();
+
+      print({_enteredEmail, _enteredPassword});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -28,6 +43,18 @@ class _LoginFormState extends State<LoginForm> {
                 'Email Address',
               ),
             ),
+            validator: (value) {
+              if (value == null ||
+                  value.trim().isEmpty ||
+                  !value.contains('@')) {
+                return "Please enter a valid email address";
+              }
+
+              return null;
+            },
+            onSaved: (newValue) {
+              _enteredEmail = newValue!;
+            },
           ),
           TextFormField(
             decoration: const InputDecoration(
@@ -36,12 +63,22 @@ class _LoginFormState extends State<LoginForm> {
               ),
             ),
             obscureText: true,
+            validator: (value) {
+              if (value == null || value.trim().length < 6) {
+                return "Password must be at least 6 characters long";
+              }
+
+              return null;
+            },
+            onSaved: (newValue) {
+              _enteredPassword = newValue!;
+            },
           ),
           const SizedBox(
             height: 12,
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: _submit,
             style: ElevatedButton.styleFrom(
               backgroundColor: Theme.of(context).colorScheme.primaryContainer,
             ),

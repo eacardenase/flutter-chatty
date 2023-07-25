16459c00214bb3a7082abe18a0d0fa55d46f05ee
Edwin Cardenas <eacardenase@unal.edu.co>
2023-07-25 14:11:32 -0500

    feat: load and display chat messages from firestore

--- lib/chat_messages.dart
+++ lib/chat_messages.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatMessages extends StatelessWidget {
   const ChatMessages({
     super.key,
@@ -7,10 +9,45 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'No messages found.',
-      ),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: false)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text(
+              'No messages found.',
+            ),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text(
+              'Something went wrong.',
+            ),
+          );
+        }
+
+        final loadedMessages = snapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (context, index) {
+            final currentMessage = loadedMessages[index].data()['text'];
+
+            return Text(currentMessage);
+          },
+        );
+      },
     );
   }
 }
